c2c2bb4fdcd7a5c45e320150fdb7d388dfa43cbc
homer1013 <homer1013@gmail.com>
2024-09-03 11:00:45 -0600

    Uppy

--- ESP32 C3/ESP32C3_Blink_LED.py
+++ ESP32 C3/ESP32C3_Blink_LED.py
@@ -2,28 +2,26 @@ import time
 from machine import Pin
 
 p8 = Pin(8, Pin.OUT)
+#p9 = Pin(9, Pin.IN)
 
-p8.off()
-time.sleep_ms(500)
-p8.on()
-time.sleep_ms(500)
-p8.off()
-time.sleep_ms(500)
-p8.on()
-time.sleep_ms(500)
-p8.off()
-time.sleep_ms(500)
-p8.on()
-time.sleep_ms(500)
-p8.off()
-time.sleep_ms(500)
-p8.on()
-time.sleep_ms(500)
-p8.off()
-time.sleep_ms(500)
-p8.on()
-time.sleep_ms(500)
-p8.off()
-time.sleep_ms(500)
-p8.on()
-time.sleep_ms(500)
+def blink():
+
+	for _ in range(0, 5): 
+		p8.on()
+		time.sleep_ms(500)
+		p8.off()
+		time.sleep_ms(500)
+		p8.on()
+		time.sleep_ms(500)
+		p8.off()
+		time.sleep_ms(500)
+		p8.on()
+		time.sleep_ms(500)
+
+#if p9.value() == 1:
+	
+blink()
+
+#else:
+
+	#None
